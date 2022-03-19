558e725dd1dd8e617cc2de2eb954c8d8fd6bdbc9
Michael Simmons <msimmons86@gmail.com>
2022-03-18 17:25:45 -0700

    commit tree 3ad74770e546b7ada1845afe5772adeab54ad51a

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= '400') {
+    return `This one is on me!`;
+  } else if (ride >= '2000' && ride < '2500') {
+    return `I will gladly take your thirty bucks.`;
+  } else if (ride >= '2500')  {
+    return `No can do.`;
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(isCity){
+  let cityNote = (isCity === `NYC`) ? (`Ok, sounds good.`) : (`No go.`);
+  return cityNote;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipp){
+  switch (tipp) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+        return "Bye.";
+      break;
+  }
 }
